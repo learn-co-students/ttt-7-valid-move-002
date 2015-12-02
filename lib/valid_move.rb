f49671bca5d8a7b5f0f110bb1a3c694762fa81d5
kyled871 <kyled871@yahoo.com>
2015-12-02 03:17:23 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,18 @@
 def valid_move?(board, position)
-  board[position.to_i-1]
+  player = position.to_i
+  if (player.between?(1,9))
+    position_taken?(board, player)
+  else
+     false
+  end
 end
+
+
+def position_taken?(board, player)
+  if (board[player - 1] == " ") || (board[player - 1] == "") || (board[player - 1] == nil)
+     true
+  else (board[player - 1] == "X") || (board[player - 1] == "O")
+     false
+  end
+end
+
