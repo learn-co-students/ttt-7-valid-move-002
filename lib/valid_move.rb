f1914d5a8d1cb1b76e6e79ef8f778da7a2d04bc5
tkendalls <director@mathplusacademy.com>
2015-12-28 20:41:47 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move? board, location
+  (0..8).cover?(location.to_i - 1) && !position_taken?(board, location.to_i - 1)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken? board, location
+  !(board[location].nil? || board[location].empty? ||  board[location] == " ")
+end
