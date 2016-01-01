995e6fe55147b4eb7e95565ffa40012315b6dae4
thegands <thegands@gmail.com>
2016-01-01 10:07:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,5 @@ end
 def position_taken?(board, pos)
   if board[pos] == "X" || board[pos] == "O" #i felt this was a better way to address edge case scenarios than what the readme suggested
     true
-  else
-    false
   end
 end
