f78350a7d33dfe39d9fbba969f8283fc98000617
bmolina-nyc <bruce.d.molina@gmail.com>
2016-01-08 15:46:57 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if board[position.to_i - 1] == " " || board[position.to_i - 1] == "" || board[position.to_i - 1] == nil && position.between?(1,9)
+  if position_taken?(board, position) == false && position.to_i.between?(0,8)
     true
-  elsif board[position.to_i - 1] == "X" || board[position.to_i - 1] == "O"
+  elsif position_taken?(board, position)
     false
   elsif position.to_i < 1 && position.to_i > 9
     false
@@ -11,9 +11,9 @@ end
 
 
 def position_taken?(board, position)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-    false
-  elsif board[position] == "X" || board[position] == "O"
-    true
-  end
+  if board[position.to_i - 1] == " " || board[position.to_i - 1] == "" || board[position.to_i - 1] == nil 
+    false  # because its blank - therefore its a valid move
+  elsif board[position.to_i - 1] == "X" || board[position.to_i - 1] == "O"
+    true # the position is taken - therefore its not a valid move
+  end 
 end 
